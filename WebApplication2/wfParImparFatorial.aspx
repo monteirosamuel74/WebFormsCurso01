461cc711b48180ae75096ff634e9e56fcbef8793
Samuel Monteiro <ss-monteiro@outlook.com>
2024-12-01 16:26:51 -0400

    Confirmacao

--- WebApplication2/wfParImparFatorial.aspx
+++ WebApplication2/wfParImparFatorial.aspx
@@ -4,13 +4,42 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+<<<<<<< HEAD
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+=======
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+>>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+<<<<<<< HEAD
         </div>
+=======
+            <asp:BulletedList ID="bltLista" runat="server" BulletStyle="Disc" DisplayMode="LinkButton" OnClick="btLista_Click">
+                <asp:ListItem>Par ou Impar</asp:ListItem>
+                <asp:ListItem>Fatorial</asp:ListItem>
+            </asp:BulletedList>
+        </div>
+        <asp:Panel ID="pnParImpar" runat="server" Visible="false">
+            <asp:Label ID="lbLabel" runat="server" Text="Par ou Ímpar"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtValorPN1" runat="server"></asp:TextBox>
+            <asp:Button ID="btnBut" runat="server" Text="Verificar" OnClick="btnBut_Click" />
+            <br />
+            <asp:Label ID="lbResp1" runat="server" Text="lable2"></asp:Label>
+        </asp:Panel>
+        <br />
+        <asp:Panel ID="pnFatorial" runat="server" Visible="false">
+            <asp:Label ID="Label1" runat="server" Text="Fatorial"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtBoxFat" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Calcular" OnClick="Button1_Click" />
+            <br />
+            <asp:Label ID="lbResp2" runat="server" Text="lable2"></asp:Label>
+        </asp:Panel>
+>>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
     </form>
 </body>
 </html>
--- WebApplication2/wfParImparFatorial.aspx.cs
+++ WebApplication2/wfParImparFatorial.aspx.cs
@@ -11,7 +11,65 @@ namespace WebApplication2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+<<<<<<< HEAD
 
+=======
+            if (!IsPostBack)
+            {
+                pnParImpar.Visible = false;
+                pnFatorial.Visible = false;
+                lbResp1.Visible = false;
+                lbResp2.Visible = false;
+            }
+        }
+
+        protected void btLista_Click(object sender, BulletedListEventArgs e)
+        {
+            switch (e.Index)
+            {
+                case 0:
+                    pnParImpar.Visible = true;
+                    break;
+                case 1:
+                    pnFatorial.Visible = true;
+                    break;
+            }
+        }
+
+        protected void btnBut_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(txtValorPN1.Text);
+            if (numero % 2 == 0)
+            {
+                lbResp1.Text = "É PAR.";
+            }
+            else
+            {
+                lbResp1.Text = "É ÍMPAR.";
+            }
+            lbResp1.Visible = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(txtBoxFat.Text);
+            int res;
+            if (numero == 0 || numero == 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                for (int i = numero; i >= 0; i--)
+                {
+                    res = numero * (numero - 1);
+                    res = res * i;
+                    numero--;
+                }
+            }
+            lbResp2.Text = Convert.ToString(res);
+            lbResp2.Visible = true;
+>>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
         }
     }
 }
