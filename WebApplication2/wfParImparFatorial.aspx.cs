d7bac74f4c867836fb146dd9265c2a5f03e073bf
Samuel Monteiro <ss-monteiro@outlook.com>
2024-12-01 17:41:27 -0400

    Feito3

--- WebApplication2/wfParImparFatorial.aspx.cs
+++ WebApplication2/wfParImparFatorial.aspx.cs
@@ -11,12 +11,6 @@ namespace WebApplication2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
             if (!IsPostBack)
             {
                 pnParImpar.Visible = false;
@@ -72,10 +66,6 @@ namespace WebApplication2
             }
             lbResp2.Text = Convert.ToString(res);
             lbResp2.Visible = true;
-<<<<<<< HEAD
->>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
-=======
->>>>>>> 690b5903d8447d70287b58fe17d69f7a69bdc7c4
         }
     }
 }
